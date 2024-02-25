7c3de7c78e6a1ad587ac10b5624559045074898a
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-02-25 23:49:04 +0530

    added booking, removing and changing methods.

--- handler.py
+++ handler.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod, ABC
 from python_datalogger import DataLogger
 
 from dataserver import DataServer
+from security import SecurityHandler
 
 
 class IBookingHandler(metaclass=ABCMeta):
@@ -19,13 +20,14 @@ class BookingHandler(IBookingHandler, ABC):
             BookingHandler()
         return BookingHandler.__instance
 
-    def __init__(self, name: str = "TestUser", row: str = None, column: str = None):
+    def __init__(self, name: str = "TestUser", password: str = "password", row: str = None, column: str = None):
         self.__logger = DataLogger(name="BookingHandler", propagate=True, level="DEBUG")
 
         if BookingHandler.__instance is not None:
             raise Exception("Cannot be instantiated more than once")
         else:
             self.__name = name
+            self.__password_hash = SecurityHandler(password=password).get_hashed_password()
             self.__row = row
             self.__column = column
             BookingHandler.__instance = self
@@ -34,7 +36,18 @@ class BookingHandler(IBookingHandler, ABC):
     def print_data(**kwargs):
         print(BookingHandler.__instance.__name)
 
+    def book_seat(self):
+        if self.__row is not None and self.__column is not None:
+            pass
+
+    def change_booking(self):
+        if self.__row is not None and self.__column is not None:
+            pass
+
+    def delete_booking(self):
+        if self.__row is not None and self.__column is not None:
+            pass
+
     @staticmethod
     def test_shit():
         DataServer()
-
