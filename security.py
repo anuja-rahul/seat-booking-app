b400bee9c7943ef46ec47775bf8b31c930f3c873
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-02-25 23:48:41 +0530

    added security and hashing for user credentials

---
+++ security.py
@@ -0,0 +1,19 @@
+
+import hashlib
+from python_datalogger import DataLogger
+
+
+class SecurityHandler:
+    def __init__(self, password: str):
+        self.__password = password
+
+    @DataLogger.logger
+    def get_hashed_password(self) -> str:
+        return hashlib.sha256(self.__password.encode()).hexdigest()
+
+    @DataLogger.logger
+    def check_password_hashes(self, password_hash: str) -> bool:
+        if self.get_hashed_password() == password_hash:
+            return True
+        else:
+            return False
